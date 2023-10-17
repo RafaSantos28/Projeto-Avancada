a8ef7ecea191ca5ad964bd5277f8a4a20a7736bc
RafaSantos28 <RRibeiro28@outlook.com>
2023-10-17 15:22:06 -0300

    RafaSantos28

--- src/main/java/io/sim/Projeto/BotPayment.java
+++ src/main/java/io/sim/Projeto/BotPayment.java
@@ -2,4 +2,22 @@ package io.sim.Projeto;
 
 public class BotPayment extends Thread {
     
+    private double valorKm = 3.25;
+    private double valorConta;
+    private double kmRodado;
+
+    public BotPayment(double valorConta, double kmRodado) {
+        this.valorConta = valorConta;
+        this.kmRodado = kmRodado;
+    }
+
+    //para o Drivers
+    public void calculoTrans(){
+        this.valorConta = this.valorConta + (this.kmRodado * this.valorKm);    
+    }
+
+    @Override
+    public void run() {
+        calculoTrans();
+    }
 }
---
+++ src/main/java/io/sim/Projeto/Principal.java
@@ -0,0 +1,10 @@
+package io.sim.Projeto;
+
+public class Principal {
+    public static void main( String[] args ) {
+
+        //EnvSimulator ev = new EnvSimulator();
+        //ev.start();
+
+    }
+}
